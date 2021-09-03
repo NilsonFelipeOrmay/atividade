d90890fa7f4f04fd48484907c4d46f1f3029ab01
NilsonFelipeOrmay <nilsonfelipe.aquino@gmail.com>
2021-09-03 17:20:56 -0400

    Atividade1

---
+++ ufc/src/ufc/Nilson.java
@@ -0,0 +1,16 @@
+
+package ufc;
+
+
+public class Nilson extends Lutadores{
+
+    public Nilson(String nome, int idade, float peso) {
+        super(nome, idade, peso);
+        
+    }
+
+  
+
+   
+    
+}
---
+++ ufc/src/ufc/Ufc.java
@@ -0,0 +1,13 @@
+
+package ufc;
+
+public class Ufc {
+
+    
+    public static void main(String[] args) {
+       
+    Nilson lutadorN = new Nilson("Nilson", 28, 80.02f);
+
+    }
+    
+}
